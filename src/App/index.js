380de997e72a136cd248bc3d3d30942c9f7a13f6
yycao <yycao@thoughtworks.com>
2017-06-14 15:46:17 +0800

    init

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import HelloReact from '../components/HelloReact';
+
+class App extends Component {
+
+    render() {
+        return (
+            <HelloReact></HelloReact>
+        );
+    }
+}
+
+export default App;
