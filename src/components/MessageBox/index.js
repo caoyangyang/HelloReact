5cd8d652d02a0a30397c321be5423a46305c161c
yycao <yycao@thoughtworks.com>
2017-06-28 16:54:18 +0800

    [Spike][MVP] lottie-react-native spike

--- src/components/MessageBox/index.js
+++ src/components/MessageBox/index.js
@@ -1,14 +1,44 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, ScrollView, ActivityIndicator} from 'react-native';
+import {StyleSheet, Text, ScrollView,Animated} from 'react-native';
 import MessageItem from '../MessageItem';
+import Animation from 'lottie-react-native';
 
 export default class MessageBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            source:require('../../animation/heart.json'),
+            progress: new Animated.Value(0),
+            config: {
+                duration: 3000,
+                imperative: false,
+            },
+        };
+    }
+
+    componentDidMount() {
+        Animated.timing(this.state.progress, {
+            toValue: 1,
+            duration: 1000,
+        }).start();
+    }
+
+    setAnim(anim) {
+        this.anim = anim;
+    }
+
     render() {
+        const {source,progress}=this.state;
         var datas = this.props.data;
         var viewMode = datas.map(m => (
             <MessageItem key={Math.random()} title={m.title} detail={m.detail} id={m.id}></MessageItem>));
-        var loaddingSpinner = <ActivityIndicator></ActivityIndicator>;
-        var messages = datas.length > 0 ? viewMode : loaddingSpinner;
+        var loaddingAnimation =<Animation
+            ref={animation => { this.animation = animation; }}
+            style={{ width: 380, height: 380}}
+            source={source}
+            progress={progress}
+            ref={this.setAnim}/>;
+        var messages = datas.length > 0 ? viewMode : loaddingAnimation;
         return (
             <ScrollView style={styles.wrapper}>{messages}</ScrollView>
         );
