e057cc6e556ae2bc1d0c7d75aeb8b840b0b600ed
yycao <yycao@thoughtworks.com>
2017-07-10 11:06:45 +0800

    [NA][Jane] add android generate part shell

---
+++ scripts/android-build-package.sh
@@ -0,0 +1,22 @@
+echo '==== start generate key ===='
+echo '==== step1: remove the old key ===='
+keytool -delete -alias hello-react-key-alias \
+-keystore hello-react-release-key.keystore \
+-storepass chnageW0rld! \
+-keypass IChangeW0rld!
+
+echo '==== step2: create new key ===='
+keytool -genkey -v \
+-keystore hello-react-release-key.keystore \
+-alias hello-react-key-alias \
+-keyalg RSA \
+-keysize 2048 \
+-validity 10000 \
+-storepass chnageW0rld! \
+-keypass IChangeW0rld! \
+-dname CN=YangYangCao,OU=TW,O=ORG,L=Wuhan,ST=Hubei,C=CN
+echo '==== end generate key end ===='
+
+echo '==== change thedirectory and generate a signed APK ===='
+cd android/ && ./gradlew assembleRelease
+
