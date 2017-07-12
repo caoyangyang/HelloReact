f895dbb53cb4d243fc05192254ab88ca5a9000c5
yycao <yycao@thoughtworks.com>
2017-07-13 00:38:05 +0800

    [NA] fix the path issue

--- scripts/android-build-package.sh
+++ scripts/android-build-package.sh
@@ -1,21 +1,22 @@
 echo '==== start generate key ===='
 echo '==== step1: remove the old key ===='
+pwd
 keytool -delete -alias hello-react-key-alias \
--keystore hello-react-release-key.keystore \
--storepass chnageW0rld! \
--keypass IChangeW0rld!
+-keystore android/app/hello-react-release-key.keystore \
+-storepass changeW0rld! \
+-keypass changeW0rld!
 
 echo '==== step2: create new key ===='
 keytool -genkey -v \
--keystore hello-react-release-key.keystore \
+-keystore android/app/hello-react-release-key.keystore \
 -alias hello-react-key-alias \
 -keyalg RSA \
 -keysize 2048 \
 -validity 10000 \
--storepass chnageW0rld! \
--keypass IChangeW0rld! \
+-storepass changeW0rld! \
+-keypass changeW0rld! \
 -dname CN=YangYangCao,OU=TW,O=ORG,L=Wuhan,ST=Hubei,C=CN
 echo '==== end generate key end ===='
 
-echo '==== change thedirectory and generate a signed APK ===='
+echo '==== change the directory and generate a signed APK ===='
 cd android/ && ./gradlew assembleRelease && cd ..
