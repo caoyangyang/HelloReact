cfd112c1c6107a7ff206b4c048e45177cecfd5ba
yycao <yycao@thoughtworks.com>
2017-07-13 15:26:07 +0800

    [NA] change scripts

--- scripts/ios-build-package.sh
+++ scripts/ios-build-package.sh
@@ -1,5 +1,12 @@
 echo '==== generate the ipa file with fastlane gem===='
+react-native bundle --dev false --platform ios --entry-file ./index.ios.js --bundle-output ./ios/main.jsbundle
+
+echo '==== generate the ipa file with fastlane gem===='
 cd ios
+gem install match gym pilot fastlane
+bundle install
+match appstore
+
 fastlane gym --export_method ad-hoc --scheme "HelloReact"
 cd ..
 
