b926f37e2a70224dec4505a9582fe0e92a8ae521
yycao <yycao@thoughtworks.com>
2017-07-13 15:43:50 +0800

    [NA]nothing

--- scripts/ios-build-package.sh
+++ scripts/ios-build-package.sh
@@ -1,12 +1,14 @@
-echo '==== generate the ipa file with fastlane gem===='
 react-native bundle --dev false --platform ios --entry-file ./index.ios.js --bundle-output ./ios/main.jsbundle
 
-echo '==== generate the ipa file with fastlane gem===='
+echo '==== change directory===='
 cd ios
+
+echo '==== generate the ipa file with fastlane gem===='
 gem install match gym pilot fastlane
 bundle install
 match appstore
-
 fastlane gym --export_method ad-hoc --scheme "HelloReact"
+
+echo '==== back to directory===='
 cd ..
 
