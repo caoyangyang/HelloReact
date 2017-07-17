9ec92015bf5b6a57d7bbba1f5368173717116393
yycao <yycao@thoughtworks.com>
2017-07-17 16:30:19 +0800

    [NA] change the main script

--- scripts/main-process.sh
+++ scripts/main-process.sh
@@ -1,8 +1,8 @@
+echo '==== update node depdency ===='
+rm -rf node_modules && npm install
+
 echo '==== build android ===='
 sh scripts/android-build-package.sh
 
 echo '==== build ios ===='
 sh scripts/ios-build-package.sh
-
-echo '==== call upload-package-to-firim script ===='
-sh scripts/upload-to-fir-im.sh
